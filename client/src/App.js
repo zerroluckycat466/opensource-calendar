a04c134a2837d676387f0b50874408a9431c870f
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-08-16 22:50:30 +0800

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Open Source Kalender</h1>
+        <p>Erforsche lokale Veranstaltungen!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
