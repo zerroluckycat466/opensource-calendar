179f9ad6477504ca3d9706400435c5d392d9f68f
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-08-16 22:50:22 +0800

    feat: initial commit for src/models/Event.js

---
+++ src/models/Event.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  date: { type: Date, required: true },
+  location: { type: String, required: true },
+  category: { type: String, required: true },
+  creator: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Event', eventSchema);
